e8f2afd84f64444eade3c54061a138595ea3f0ac
RafaelHGS <rafaelhenrique43@yahoo.com.br>
2025-05-02 17:45:53 -0300

    Set Operacoes Basicas

---
+++ src/set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,27 @@
+package set.OperacoesBasicas;
+
+public class Convidado {
+    private String nome;
+    private int codConvite;
+
+    public Convidado(String nome, int codConvite) {
+        this.nome = nome;
+        this.codConvite = codConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodConvite() {
+        return codConvite;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codConvite=" + codConvite +
+                '}';
+    }
+}
